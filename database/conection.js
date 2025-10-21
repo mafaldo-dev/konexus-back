d17bc5e7ebecf31b1a43d0a6ad092f0f400653cb
Guilherme <03.09gui.mafaldo@gmail.com>
2025-10-20 22:16:13 -0300

    fix(nova correçao de cors)

--- database/conection.js
+++ database/conection.js
@@ -6,14 +6,10 @@ dotenv.config({ path: "../.env" });
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USERNAME,
-  password: process.env.PG_PASSWORD,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 5000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
