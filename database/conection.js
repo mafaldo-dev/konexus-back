73208cdddf1f33a840cee8a0a5cb6be41d9e67e1
Guilherme <03.09gui.mafaldo@gmail.com>
2025-10-29 21:01:42 -0300

    fix

--- database/conection.js
+++ database/conection.js
@@ -4,21 +4,22 @@ import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 
 const { Pool } = pkg;
-{/**/}
+{/*
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
+*/}
 
 
-{/*
+{/**/}
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL_LOCAL || "postgresql://postgres:!devgui123@@db:5432/guiman",
 });
 
 console.log("[DB] Conectado ao banco LOCAL (container Docker)");
-*/}
+
 
 export default pool;
